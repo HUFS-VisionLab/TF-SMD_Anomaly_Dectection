22e8f7680b789d6b11ccfed1b9ad5093d7dc9060
Nam Ki Hyun <devkihyun@naver.com>
2019-11-27 17:58:08 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import json
 import shutil
 import argparse
 
@@ -7,54 +8,27 @@ import numpy as np
 import tensorflow as tf
 import torch
 
+from utils.metric import get_score
 from plot import save_loss, compare_loss 
-from model import Model
-
 from datasets_loader import DatasetsLoader
 
-
-def calc_roc(score, target_name, min_max):
-    fpr_list = []
-    tpr_list = []
-    min_threshold, max_threshold = min_max
-    interval = (max_threshold - min_threshold) / 20
-    
-    thres = min_threshold
-    for i in range(21):
-        true_negative = 0
-        all_negative = 0
-        for name, loss_list in score.items():
-            if  name == target_name:
-                if len(args.targets) == 1:
-                    tpr_list.append(len([loss for loss in loss_list if loss < thres]) / len(loss_list))
-            else:
-                all_negative += len(loss_list)
-                true_negative += len([loss for loss in loss_list if loss > thres])
-        fpr_list.append(1-(true_negative/all_negative))
-        
-        thres += interval
+from model import Model
 
     
-    roc = np.array([fpr_list, tpr_list])
-    return roc
-
-    
-        
-
 if __name__ == '__main__' :
     parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--targets', nargs='*', default=None, help='-')
     parser.add_argument('--n_layers', type=int, default=3, help='-')
     parser.add_argument('--model_type', type=int, default=2, help='-')
     parser.add_argument('--no_bidirectional', action='store_true', default=False, help='-')
     parser.add_argument('--data_type', type=str, default='timesteps_64_mel_80', help='-')
-    parser.add_argument('--augment', action='store_true', default=False, help='-')
+    parser.add_argument('--augment', type=str, default='00', help='-')
     parser.add_argument('--learning_rate', type=float, default=0.005, help='-')
     parser.add_argument('--beta_1', type=float, default=0.9, help='-')
     parser.add_argument('--beta_2', type=float, default=0.999, help='-')
     parser.add_argument('--epsilon', type=float, default=1e-08, help='-')
     parser.add_argument('--epochs', type=int, default=3000, help='-')
     parser.add_argument('--batch_size', type=int, default=None, help='-')
-    parser.add_argument('-l', '--targets', nargs='*', default=None, help='-')
     parser.add_argument('--model_path', type=str, default='./model', help='-')
     parser.add_argument('--inference', action='store_true', default=False, help='-')
     parser.add_argument('--loss_log_scale', type=int, default=0, help='-')
@@ -62,7 +36,7 @@ if __name__ == '__main__' :
     
     
     """ Preprare the path of dataset and load hyper parameter of model """
-    datasets_loader = DatasetsLoader(targets=args.targets, data_type=args.data_type, augment=args.augment)
+    datasets_loader = DatasetsLoader(targets=args.targets, data_type=args.data_type, is_augment=args.augment)
     data_version = datasets_loader.version
     trainPath_dict = datasets_loader.pathList_dict['train']
     testPath_dict = datasets_loader.pathList_dict['test']
@@ -78,24 +52,22 @@ if __name__ == '__main__' :
     args.model_type = modelType_dict[args.model_type]
     
     
+    augment_detail = {'0' : 'normal', '1' : 'shifted', '2' : 'both'}
+    is_trainAug = augment_detail[args.augment[0]]
+    is_testAug = augment_detail[args.augment[1]]
+    
+    
     """ Save path of result """
     model_path       = './model'
     figure_path      = './figure'
     model_name       = 'Bi' + args.model_type if args.no_bidirectional != True else args.model_type
     model_name       = f'{model_name}_{args.n_layers}_{args.epochs}'
-    target_name      = "_".join(args.targets)
-    if args.augment == True:
-        args.save_path   = os.path.join(model_path, str(data_version), model_name,  f'{args.data_type}_shifted')
-        save_figure_path = os.path.join(figure_path, str(data_version), model_name, f'{args.data_type}_shifted')
-    else:
-        args.save_path   = os.path.join(model_path, str(data_version), model_name,  f'{args.data_type}')
-        save_figure_path = os.path.join(figure_path, str(data_version), model_name, f'{args.data_type}')
-        
+    target_name      = "_".join(args.targets) # EX) args.targets = [Class0, Class1] --> target_name : 'Class0-name_Class1-name'
+    
+    args.save_path   = os.path.join(model_path, str(data_version), model_name,  f'{args.data_type}_train_{is_trainAug}')
     os.makedirs(args.save_path, exist_ok=True)
-    os.makedirs(save_figure_path, exist_ok=True)
     args.save_path   = os.path.join(args.save_path, target_name)
-    save_figure_path = os.path.join(save_figure_path, target_name)
-
+    
     if args.inference == False:
         with tf.Graph().as_default():
             model = Model(args)
@@ -106,6 +78,10 @@ if __name__ == '__main__' :
                 save_loss(model.loss_list,  args.save_path)
     else:
         with tf.Graph().as_default():
+            save_figure_path = os.path.join(figure_path, str(data_version), model_name, f'{args.data_type}_train_{is_trainAug}_test_{is_testAug}')
+            os.makedirs(save_figure_path, exist_ok=True)
+            save_figure_path = os.path.join(save_figure_path, target_name)
+            
             model = Model(args)
             
             model.load_weights()
@@ -114,19 +90,16 @@ if __name__ == '__main__' :
 
             # Evaluate
             loss_dict = {}
-            threashold = None
 
             max_loss = 0 
             for name, path_list in testPath_dict.items():
                 loss_list = model.inference(path_list)
-                max_loss = max(max_loss, np.max(loss_list))
 
                 if name == target_name:
-                    min_loss = np.min(loss_list)
                     avg_loss = np.mean(loss_list)
-                    threshold = avg_loss * 1.2
                     if len(args.targets) == 1:
-                        name = datasets_loader.datasets[name]['name'] 
+                        name = datasets_loader.datasets[name]['name']
+                        target_name = name
                 else: 
                     # If not trained target, use full name. Not important
                     name =datasets_loader.datasets[name]['name']
@@ -135,6 +108,7 @@ if __name__ == '__main__' :
                 
             
             compare_loss(loss_dict,  args.save_path, avg_loss, save_figure_path, log_scaling=args.loss_log_scale)
-             
-            """roc = calc_roc(loss_dict, datasets_loader.datasets[target_name]['name'] , min_max=(min_loss, max_loss)) # evaluate with ROC
-            np.save(f'{save_figure_path}_roc.npy', roc)"""
+            score_dict = get_score(loss_dict, target_name, args.timesteps, args.inputs_dims)
+            with open(f"{save_figure_path}_score.json", 'w', encoding='utf-8') as f:
+                json.dump(score_dict, f, ensure_ascii=False, indent=4)
+            
