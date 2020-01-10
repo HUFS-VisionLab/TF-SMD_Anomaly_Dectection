307cf1bacd7c9cf5dd65aefea80b3a116734c7d1
Nam Ki Hyun <devkihyun@naver.com>
2020-01-10 15:09:39 +0900

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -1,21 +1,9 @@
 class hparams:
-    # Hyper Parameter for preprocessing
-    data_type = 'mel'
-    seq_length = 64
-    timeshift = 0.5 # 500ms
-
-    # Hyper Parameters for Audio 
+    # Hyper Parameters for SMD audio file 
     sample_rate = 192 * 1000
     preemphasis = 0.97
-    window_length = 0.02 #20ms
-    window_stride = 0.01 #10ms
+    window_length = 0.01 #10ms
+    window_stride = window_length / 4 # 5ms, 25% over-lapping
     n_fft = int(sample_rate * window_length) 
     win_length = n_fft
     hop_length = int(sample_rate * window_stride) 
-    n_mels = 80
-    n_mfcc = 60
-    min_level_db = -100
-    ref_level_db = 20
-    max_iters=200
-    griffin_lim_iters=60
-    power=1.5  
